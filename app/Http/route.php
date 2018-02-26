7828f37029a1ea9801d703356b1b923bab28d63e
mikaellemos033 <mikaellemos033@gmail.com>
2018-02-25 21:05:00 -0300

    run application

---
+++ app/Http/route.php
@@ -0,0 +1,7 @@
+<?php 
+
+$route = new Sect\Http\Routing\Route();
+
+$route->get('/', function() {
+	return 'fucking the police';
+});
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,10 +1,14 @@
 <?php
 
+require ('../vendor/autoload.php');
+
 define('BASE', __DIR__ . '/../');
 define('SECT_BASE', sprintf('%s/../vendor/mikaellemos033/santo/src/Santo', __DIR__));
 
-$config = new Sect\Config\Raw(SECT_BASE . '/Fire'));
-
-
+$config   = new Sect\Config\Raw(SECT_BASE . '/Fire');
 $provider = new Sect\Providers\KickProvider();
+
 $provider->handle(array_merge($config->fire('Providers'), $config->config(BASE . '/config/Providers')));
+
+require (BASE . '/app/Http/route.php');
+return print($route->run());
